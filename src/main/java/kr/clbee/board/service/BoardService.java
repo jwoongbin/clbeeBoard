b23e00b82f357443499c2d33f54e12f16a794e09
woongbinJung <philoleben@gmail.com>
2020-10-23 15:41:49 +0900

    2번째 커밋

--- src/main/java/kr/clbee/board/service/BoardService.java
+++ src/main/java/kr/clbee/board/service/BoardService.java
@@ -1,11 +1,15 @@
 package kr.clbee.board.service;
 
+import kr.clbee.board.domain.Entity.BoardEntity;
 import kr.clbee.board.dto.BoardDto;
 import kr.clbee.board.domain.repository.BoardRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @AllArgsConstructor
 @Service
@@ -13,7 +17,49 @@ public class BoardService {
     private BoardRepository boardRepository;
 
     @Transactional
+    public List<BoardDto> getBoardlist() {
+        List<BoardEntity> boardEntities = boardRepository.findAll();
+        List<BoardDto> boardDtoList = new ArrayList<>();
+
+        for (BoardEntity boardEntity : boardEntities) {
+            BoardDto boardDTO = BoardDto.builder()
+                    .id(boardEntity.getId())
+                    .title(boardEntity.getTitle())
+                    .content(boardEntity.getContent())
+                    .writer(boardEntity.getWriter())
+                    .createdDate(boardEntity.getCreatedDate())
+                    .build();
+
+            boardDtoList.add(boardDTO);
+        }
+
+        return boardDtoList;
+    }
+
+    @Transactional
+    public BoardDto getPost(Long id) {
+        Optional<BoardEntity> boardEntityWrapper = boardRepository.findById(id);
+        BoardEntity boardEntity = boardEntityWrapper.get();
+
+        BoardDto boardDTO = BoardDto.builder()
+                .id(boardEntity.getId())
+                .title(boardEntity.getTitle())
+                .content(boardEntity.getContent())
+                .writer(boardEntity.getWriter())
+                .createdDate(boardEntity.getCreatedDate())
+                .build();
+
+        return boardDTO;
+    }
+
+    @Transactional
     public Long savePost(BoardDto boardDto) {
         return boardRepository.save(boardDto.toEntity()).getId();
     }
+
+    @Transactional
+    public void deletePost(Long id) {
+        boardRepository.deleteById(id);
+    }
+
 }
